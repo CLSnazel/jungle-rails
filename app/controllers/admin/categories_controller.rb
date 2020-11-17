115d55e36616ff1066d7c135ee6e44945032eea7
Chantal Snazel <clsnazel@gmail.com>
2020-11-17 07:05:26 +0000

    add admin/categories routes and controller

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,15 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    raise 'hello'
+  end
+  
+  def new
+    raise 'new'
+  end
+
+  def create
+    raise 'create'
+  end
+
+
+end
