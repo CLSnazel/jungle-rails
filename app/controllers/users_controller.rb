49155725f6ec5a7eb585237c1ae49ae34cd3a855
Chantal Snazel <clsnazel@gmail.com>
2020-11-17 08:51:58 +0000

    add signup page, add user#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
+ 
   def new
   end
 
   def create
+      
+    user = User.new(user_params)
+    # raise 'got to here'
+    if user.save
+      session[:user_id] = user.id
+      redirect_to [:products], notice: 'Sign up complete!'
+    else
+      redirect_to '/signup'
+    end
   end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+
 end
