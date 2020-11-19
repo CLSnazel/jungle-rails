2d60ce9b75f1a934866a8e3caa0743846222f05d
Chantal Snazel <clsnazel@gmail.com>
2020-11-19 02:21:42 +0000

    add auth_w/_cred function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,22 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence:true
   validates :last_name, presence:true
-  validates :email, presence:true, uniqueness: {case_sensitive: false}
+  validates :email, uniqueness: {case_sensitive: false, message:"already registered"}, presence:true 
   validates :password, presence:true, confirmation:true, length: {minimum: 5}
   validates :password_confirmation, presence:true
+
+  before_save do self.email.downcase! end
+  
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    
+    # If the user does not exist or does not authenticate
+    if !user || !user.authenticate(password)
+      user = nil
+    end
+
+    return user
+  end
+
 end
