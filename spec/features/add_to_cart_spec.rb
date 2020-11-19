cadbb6b5f697d135c41e0eab7d17e37626175fef
Chantal Snazel <clsnazel@gmail.com>
2020-11-19 06:37:00 +0000

    fill in add cart test

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "they can click on add to add item to cart" do 
+    visit root_path
+
+    # cart = first('a[href="cart"')
+    expect(page).to have_text 'My Cart (0)'
+
+    first('article footer form button').click
+
+    expect(page).to have_text 'My Cart (1)'
+  end
 end
