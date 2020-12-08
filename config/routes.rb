46199f4b5599f512d426b36b42ee47d899b33d18
Chantal Snazel <clsnazel@gmail.com>
2020-12-08 20:02:52 +0000

    fix routing issues between / and /admin

--- config/routes.rb
+++ config/routes.rb
@@ -24,13 +24,19 @@ Rails.application.routes.draw do
 
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  resource :users, path:'/signup' do
+    root to: 'users#new'
+    post :create
+  end
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-
+  resource :sessions, path:'/login' do
+    root to: 'sessions#new'
+    post :create
+  end
+  
+  resource :logout do
+    root to: 'sessions#destroy'
+  end
   # resource :about, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
